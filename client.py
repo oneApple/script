b4185f7fb5e17aaf49904af75d090c3e1b18c8c8
oneApple <710406979@qq.com>
2013-03-26 21:44:14 +0800

    make,pack

--- client.py
+++ client.py
@@ -7,7 +7,14 @@ s.connect(('127.0.1.1',1234))
 string = s.recv(20)
 print struct.unpack("qqi",string)
 while string:
+<<<<<<< HEAD
     s.send(struct.pack("qqi",*struct.unpack("qqi",string)))
     string = s.recv(20)
     print struct.unpack("qqi",string)
     time.sleep(5)
+=======
+    s.send(struct.pack("ii",4004,0))
+    string = s.recv(12)
+    print struct.unpack("iii",string)
+    time.sleep(10)
+>>>>>>> 57d31e8... modify client.py
