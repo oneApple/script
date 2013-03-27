c419e66c9dcc61748565945437f8b9e3ffbbefed
keyaming <keyamingcuc@gmail.com>
2013-03-27 17:45:32 +0800

    modify msg size

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import socket
 import struct
 s = socket.socket()
 s.connect(('127.0.1.1',1234))
-string = s.recv(12)
-print struct.unpack("iii",string)
+string = s.recv(20)
+print struct.unpack("qqi",string)
 while string:
-    s.send(struct.pack("ii",4004,0))
-    string = s.recv(12)
-    print struct.unpack("iii",string)
-    time.sleep(10)
+    s.send(struct.pack("qqi",*struct.unpack("qqi",string)))
+    string = s.recv(20)
+    print struct.unpack("qqi",string)
+    time.sleep(5)
